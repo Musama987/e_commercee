42ac6a7ad8bc970a6f4ab971ae27ad86637d844b
M Usama Ramzan <usamaramzan215@gmail.com>
2025-10-09 20:01:28 +0500

    feat: integrate Firebase into Android app
    Integrates Firebase with the Android application by adding the `google-services` Gradle plugin and updating the `.gitignore` to exclude Firebase configuration files.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:e_commercee/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'firebase_options.dart';
 
 
-void main() {
+Future<void> main() async{
   ///widget flutter binding
-  // final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
   /// flutter native splash
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   //portrait up the screen
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
